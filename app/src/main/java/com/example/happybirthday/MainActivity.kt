b2a40429d06079f3a3978a7f64414e34cac72870
das-dipankar <91499712+das-dipankar@users.noreply.github.com>
2024-04-13 05:13:47 +0530

    alignments updated

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.happybirthday
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -13,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,15 +46,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Text(
-        text = message,
-        fontSize = 100.sp,
-        lineHeight = 112.sp,
-    )
-    Text(
-        text = from,
-        fontSize = 36.sp
-    )
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = message,
+            fontSize = 101.sp,
+            lineHeight = 112.sp,
+            color = Color.Red,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.background(color = Color.LightGray)
+        )
+        Text(
+            text = from,
+            fontSize = 30.sp,
+            lineHeight = 35.sp,
+            modifier = Modifier
+                .background(color = Color.DarkGray)
+                .padding(16.dp)
+                .align(Alignment.End)
+        )
+    }
 
 }
 
